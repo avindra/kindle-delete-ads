068dbda57ae1bbe922d415a40407e62f8e69450a
Avindra Goolcharan <aavindraa@gmail.com>
2024-06-20 20:49:07 -0400

    adapt for Jun 2018 systemd changes

--- README.md
+++ README.md
@@ -2,13 +2,12 @@
 
 ## Installation
 
-The recommended installation is to the `bin` folder of your regular user's home folder, as this should be in your `$PATH` by default.
+The recommended installation is to your local `bin` folder (it should be in your `$PATH` by default).
 
 ```bash
 wget -P ~/bin https://raw.githubusercontent.com/avindra/kindle-delete-ads/master/kindle-delete-ads.sh
 chmod +x ~/bin/kindle-delete-ads.sh
 ```
-
 
 ## Usage
 
@@ -46,9 +45,13 @@ Deleting the `.assets` folder will work, but is only a temporary fix. I would re
 
 # Autorun on plug in
 
-You can really make life as easy as possible by having your computer wipe the Kindles ads every time you plug it in. Add a file to `/etc/udev/rules.d` called `kindle.rules`:
+To make life as easy as possible, have your computer wipe the Kindles ads every time you plug it in.
 
-```
+Unfortunately [since June 2018, udevd service has been restricted to a limited set of syscalls](https://github.com/systemd/systemd/commit/ee8f26180d01e3ddd4e5f20b03b81e5e737657ae#r143372255), notably missing `@mount`. To work around this, add `@mount` to `SystemCallFilter` in `/usr/lib/systemd/system/systemd-udevd.service`, or simply comment out the entire `SystemCallFilter` line.
+
+Afterwards, add a file to `/etc/udev/rules.d` called `99-kindle.rules`.
+
+```sh
 ACTION=="add", ENV{DEVTYPE}=="partition", ATTRS{idVendor}=="1949", ATTRS{idProduct}=="0004", RUN+="/home/avindra/bin/kindle-delete-ads.sh '%E{DEVNAME}'"
 ```
 
--- kindle-delete-ads.sh
+++ kindle-delete-ads.sh
@@ -1,11 +1,22 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
-if [[ $EUID -ne 0 ]]; then
+# to debug, uncomment the following 2 lines
+# exec 1> /tmp/kindle-ads 2>&1
+# set -x
+
+#
+# changelog
+#
+#  2024-06-20: drop bashims, use standard shell
+#  2016-07-24: initial release
+
+
+if [ $EUID -ne 0 ]; then
 	echo "Run this script with Root priveleges."
 	exit 1
 fi
 
-if [[ -n "$1" ]]; then
+if [ -n "$1" ]; then
 	kindleDevice="$1"
 else
 	echo -n "Autodetecting Kindle device... "
@@ -16,16 +27,24 @@ fi
 echo "Kindle device file is $kindleDevice"
 
 mountInfo=`df | grep $kindleDevice`
-if [[ $? -eq 0 ]]; then
+if [ $? -eq 0 ]; then
 	echo "Kindle is already mounted! unmounting...."
 	umount $kindleDevice
 fi
 
-
 tmpMount=`mktemp -d`
-echo "Mounting Kindle to temporary location: $tmpMount"
+
+# note: usage of "mount" here requires explicit permission grant
+# in the udevd service file.
 mount $kindleDevice $tmpMount
 
+# the suggested mount method doesn't work (hangs up)
+# systemd-mount --no-ask-password --no-pager --fsck=false --automount=false $kindleDevice $tmpMount
+
+if [ $? -ne 0 ] ; then
+	echo "Failed to mount fs"
+	exit 1
+fi
 
 # We are in Kindle filesystem after this point...
 
@@ -33,9 +52,12 @@ cd $tmpMount
 
 adverts="system/.assets"
 
-if [[ -f $adverts ]]; then
+echo "Mounting Kindle to temporary location: $tmpMount"
+
+
+if [ -f $adverts ]; then
 	echo "Temp ad blocker is still intact! Nothing to do."
-elif [[ -d $adverts ]]; then
+elif [ -d $adverts ]; then
 	echo "Ads detected. Cleaning time."
 
 	echo -n "Deleting all ads... "
@@ -47,9 +69,7 @@ elif [[ -d $adverts ]]; then
 	echo "done"
 fi
 
-
 # Post-cleanup for this utility
-
 
 echo -n "Unmounting kindle... "
 
